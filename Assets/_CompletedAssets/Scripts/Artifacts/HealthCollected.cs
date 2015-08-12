477104efdfaf7f7bfe2a31fe5e35f8371c7c9aed
gilgulim <gilperetz2002@gmail.com>
2015-08-12 23:45:12 +0300

    init commit

---
+++ Assets/_CompletedAssets/Scripts/Artifacts/HealthCollected.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CompleteProject
+{
+    public class HealthCollected : MonoBehaviour
+    {
+        public const int ARTIFACT_AMOUNT = 25;
+        GameObject player;                          // Reference to the player GameObject.
+        PlayerHealth playerHealth;
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            // If the entering collider is the player...
+            if (other.gameObject == player)
+            {
+                playerHealth.Regeneration(ARTIFACT_AMOUNT);
+                Destroy(gameObject);
+            }
+        }
+
+        void Awake()
+        {
+            // Setting up the references.
+            player = GameObject.FindGameObjectWithTag("Player");
+            playerHealth = player.GetComponent<PlayerHealth>();
+        }
+
+    }
+
+}
+
