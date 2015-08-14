6368d12e219b95069e3478c3c75e81bdbf40654c
gilgulim <gilperetz2002@gmail.com>
2015-08-14 11:22:37 +0300

    Add walls

---
+++ Assets/SciFi_Door/Script/Door1.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door1 : MonoBehaviour {
+    GameObject door;
+    Animation anim;
+    AnimationClip clip;
+    void Awake()
+    {
+        
+    }
+    
+    // Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        door = GameObject.FindGameObjectWithTag("SF_Door1");
+        
+        anim = door.GetComponent<Animation>();
+        anim.Play("open");
+        
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        door = GameObject.FindGameObjectWithTag("SF_Door1");
+        anim = door.GetComponent<Animation>();
+        anim.Play("close");
+    }
+}
